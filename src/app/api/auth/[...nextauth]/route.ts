51cdd1f0219784351239fb7c73d05eaa83719c87
pockx96 <pock.kriz@gmail.com>
2024-08-24 09:04:46 -0700

    fix prisma next auth adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +13,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 });
 
 export { handler as GET, handler as POST };
