fa0732baf79204447f863f2ded5d49f6788008bc
LordHikarin <andres_g20@outlook.com>
2024-12-10 16:20:48 -0700

    Zod Validations

--- src/validations/clientSchema.ts
+++ src/validations/clientSchema.ts
@@ -8,20 +8,24 @@ export const clientSchema = z.object({
   name: z.string().min(1, "El nombre es requerido"),
   address: z.string().min(1, "La dirección es requerida"),
   phone: z.string().min(1, "El teléfono es requerido"),
-  creditLimit: z.number(), // Usando decimalSchema para límite de crédito
-  currentBalance: z.number(), // Usando decimalSchema para saldo actual
-  email: z.string().email("Email inválido"), // Validación de correo electrónico
-  debts: z
-    .array(
-      z.object({
-        id: z.number().int().positive().optional(),
-        amount: z.number(),
-        isPaid: z.boolean(),
-        clientId: z.number().int().positive().optional(),
-        saleId: z.number().int().positive().optional(),
+  creditLimit: z.number().refine((value) => parseFloat(value.toFixed(2)), {
+    message: "El límite de crédito debe tener hasta 2 decimales",
+  }),
+  currentBalance: z.number().refine((value) => parseFloat(value.toFixed(2)), {
+    message: "El saldo actual debe tener hasta 2 decimales",
+  }),
+  email: z.string().email("Email inválido"),
+  debts: z.array(
+    z.object({
+      id: z.number().int().positive().optional(),
+      amount: z.number().refine((value) => parseFloat(value.toFixed(2)), {
+        message: "El monto debe tener hasta 2 decimales",
       }),
-    )
-    .optional(), // Optional debts array
+      isPaid: z.boolean(),
+      clientId: z.number().int().positive().optional(),
+      saleId: z.number().int().positive().optional(),
+    })
+  ).optional(),
 });
 
 export type ClientType = z.infer<typeof clientSchema>;
