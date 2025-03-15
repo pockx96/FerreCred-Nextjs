64d7c40554f4e9aca23ff28979bd174fdfe53f26
pockx96 <pock.kriz@gmail.com>
2025-03-15 09:54:00 -0700

    fix production home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,20 @@
-
 import { getServerAuthSession } from "~/server/auth";
 import AdminPanelLayout from "../_components/admin-panel/admin-panel-layout";
 import { SellCart } from "../_components/sell-car/sell-cart";
 
-const HomePage = async ({ children }: { children: React.ReactNode }) => {
-    const session = await getServerAuthSession();
+export default async function HomePage() {
+  const session = await getServerAuthSession();
   return (
     <AdminPanelLayout>
       <p className="text-center text-2xl text-orange-500">
-              {session && <span>Logged in as <br></br>{session.user?.name}</span>}
-            </p>
-      <SellCart/>
+        {session && (
+          <span>
+            Logged in as <br></br>
+            {session.user?.name}
+          </span>
+        )}
+      </p>
+      <SellCart />
     </AdminPanelLayout>
   );
-};
-
-export default HomePage;
+}
