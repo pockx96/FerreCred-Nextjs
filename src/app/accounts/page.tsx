62ecf831ef06662181a00078d3ccff4199ba7804
pockx96 <pock.kriz@gmail.com>
2025-03-15 09:36:47 -0700

    fix production

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -1,12 +1,10 @@
 import { AccountClient } from "../_components/accounts/accounts-client";
 import AdminPanelLayout from "../_components/admin-panel/admin-panel-layout";
 
-const AccountsPage = ({ children }: { children: React.ReactNode }) => {
+export default function AccountsPage() {
   return (
     <AdminPanelLayout>
       <AccountClient />
     </AdminPanelLayout>
   );
-};
-
-export default AccountsPage;
+}
